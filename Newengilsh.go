2ce4536e6db0048d7a1ea96ab8af5dd8a76b4d5a
rzry <1319339638@qq.com>
2019-04-08 09:44:49 +0800

    Ch_Problem

---
+++ Newengilsh.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func HttpGetDB(url string) (result string, err error) {
+	resp, err1 := http.Get(url)
+	if err1 != nil {
+		err = err1
+		return
+	}
+	defer resp.Body.Close()
+
+	buf := make([]byte, 4096)
+
+	for {
+		n, err2 := resp.Body.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err2 != nil && err2 != io.EOF {
+			err = err2
+			return
+		}
+		result += string(buf[:n])
+	}
+	return
+}
+func Save2file(idx int, englishname [][]string) {
+	path := "/home/rzry/桌面/" + "第 " + strconv.Itoa(idx) + " 页.txt"
+	f, err := os.Create(path)
+	if err != nil {
+		fmt.Println("os err = ", err)
+		return
+	}
+	defer f.Close()
+	n := len(englishname)
+	for i := 0; i < n; i++ {
+
+		f.WriteString(englishname[i][1])
+		f.WriteString("\n")
+	}
+
+}
+func SpiderPageDB(idx int, page chan int) {
+	url := "https://www.koolearn.com/dict/tag_1395_" + strconv.Itoa(idx) + ".html"
+
+	//	fmt.Println(url)
+	result, err := HttpGetDB(url)
+
+	if err != nil {
+		fmt.Println("err = ", err)
+		return
+	}
+
+	ret1 := regexp.MustCompile(`<a class="word" href="/dict/wd_(.*?).html`)
+
+	englishname := ret1.FindAllStringSubmatch(result, -1)
+
+	Save2file(idx, englishname)
+
+	page <- idx
+}
+
+func toWork(start, end int) {
+	fmt.Printf("正在爬取...")
+	page := make(chan int)
+
+	for i := start; i <= end; i++ {
+		go SpiderPageDB(i, page)
+		for i := start; i <= end; i++ {
+			fmt.Printf("第%d 爬取完毕\n", <-page)
+		}
+	}
+}
+
+func main() {
+	var start, end int
+
+	fmt.Print("输出起始页")
+
+	fmt.Scan(&start)
+
+	fmt.Print("输入终止")
+
+	fmt.Scan(&end)
+
+	toWork(start, end)
+}
